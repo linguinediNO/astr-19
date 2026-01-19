253bebff824984963451b21d71f4fc7c9fc86def
Yadira Lorenzo <yalorenz@ucsc.edu>
2026-01-19 01:05:50 -0800

    journal 2

---
+++ journal2.py
@@ -0,0 +1,20 @@
+#Sum of two floating point numbers
+a = 1.5
+b = 2.5
+sum_result = a + b
+print("Sum:", sum_result)
+print("Type:", type(sum_result))
+
+#Difference between two intergers 
+x = 10
+y = 3
+difference_result = x - y
+print("Difference:", difference_result)
+print("Type:", type(difference_result))
+
+#Product of a floating number and an integer
+m = 2.0
+n = 4
+product_result = m * n
+print("Product:", product_result)
+print("Type:", type (product_result))
