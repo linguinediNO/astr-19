0b2c27519bb0d0f1b6d12e91bf41b1e0f53513ba
Yadira Lorenzo <yalorenz@ucsc.edu>
2026-01-19 00:50:05 -0800

    updated journal 1

--- journal1.py
+++ journal1.py
@@ -1,1 +1,3 @@
-print("my name is yadira")
+print("Hello! My name is Yadira Lorenzo.")
+print("I don’t have a favorite movie, but I really enjoyed Incident in a Ghostland and The Ring, and I recommend them often.")
+print("I LOVE meat, espically beef. Although, I also enjoy eating red rice (a Mexican dish).")
